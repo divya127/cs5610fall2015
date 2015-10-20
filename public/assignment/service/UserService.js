4d6aa7ff8960053f70a3189a872d670b09d099cb
Divya Devaraj <divya@Divyas-MacBook-Pro.local>
2015-10-19 23:39:44 -0700

    updated index.html

--- public/assignment/service/UserService.js
+++ public/assignment/service/UserService.js
@@ -4,8 +4,19 @@
         .factory("UserService", UserService);
 
     function UserService() {
-        var id1 = Guid.create();
-        var id2 = Guid.create();
+
+        function guid() {
+          function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+              .toString(16)
+              .substring(1);
+          }
+          return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+            s4() + '-' + s4() + s4() + s4();
+        }
+
+        var id1 = guid();
+        var id2 = guid();
         var users = [
             {username: "divya12", password: "test123", id: id1 , email: "divya@gmail.com"},
             {username: "admin007", password: "ALX2314", id: id2, email: "admin@gmail.com"}

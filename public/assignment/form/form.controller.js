6f794b0c8872da371f4715be636330f204d5c52d
Divya Devaraj <divya@Divyas-MacBook-Pro.local>
2015-10-24 02:25:21 -0700

    Form Requirements commit

--- public/assignment/form/form.controller.js
+++ public/assignment/form/form.controller.js
@@ -2,18 +2,54 @@
     'use strict';
          angular
              .module("FormBuilderApp")
-             .controller("RegisterController", RegisterController);
+             .controller("FormController", FormController);
 
-         function RegisterController($scope, UserService, $location, $rootScope) {
+         function RegisterController($scope, UserService, $location, $rootScope, FormService) {
 
-             $scope.allUsers = UserService.findAllUsers();
-             $rootScope.user = { };
+             FormService.findAllFormsForUser(userId, getallForms);
+             function getallForms(response) {
+                if(response != null) {
+                    $scope.forms = response;
+                }
+             }
 
+             $scope.addForm = function() {
+                var formObj = { };
+                FormService.createFormForUser($rootScope.curid, formObj, createForm);
+             }
+             function createForm(response) {
+                if(response != null) {
+                    $scope.forms = response;
+                }
+             }
 
-             $scope.register = function($rootScope){
-                $rootScope.user = UserService.createUser($scope.user.username, $scope.user.pwd, $scope.user.email);
-                $scope.$location.url("/profile");
-               }
+             $scope.updateForm = function() {
+                FormService.updateFormById(formId, newForm, updateForm);
+             }
+             function updateForm(response) {
+                 if(response != null) {
+                     $scope.forms = response;
+                 }
+             }
+
+             $scope.deleteForm = function(index) {
+                var currentForm = $scope.forms[index];
+                 FormService.deleteFormById(currentForm.id, deleteForm);
+             }
+             function deleteForm(response) {
+                  if(response != null) {
+                      $scope.forms = response;
+                  }
+             }
+
+             $scope.selectForm = function(index) {
+                  $scope.currentForm = $scope.forms[index];
+             }
+             function selectForm(response) {
+                  if(response != null) {
+                      $scope.forms = response;
+                  }
+             }
 
          }
      })();
--- public/assignment/service/FormService.js
+++ public/assignment/service/FormService.js
@@ -6,9 +6,21 @@
 
     function FormService() {
 
+        function uniqueIdForm() {
+          function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+              .toString(16)
+              .substring(1);
+          }
+          return s4() + s4() + '-' + s4();
+        }
+
+        var id1 = uniqueIdForm();
+        var id2 = uniqueIdForm();
+
         var forms = [
-            {username: "divya12", password: "test123", id: id1 , email: "divya@gmail.com"},
-            {username: "admin007", password: "ALX2314", id: id2, email: "admin@gmail.com"}
+            {formId: id1, name: "Registration Form", userid: "ff51fb46-6251-a9ef-79be-7264a076562c"},
+            {formId: id2, name: "Todo List", userid: "3b0d054b-d879-1b72-328a-7ad92d25a5b2"}
         ];
 
         var service = {
@@ -20,19 +32,43 @@
         return service;
 
         function createFormForUser(userId, form, callback) {
-            return users;
+            var id = uniqueId();
+            var newForm = {formId: id, name: form.name, userid: userId};
+            forms.push(newForm);
+            callback(newForm);
         }
 
         function findAllFormsForUser(userId, callback) {
-
-            return forms;
+            var formsForUser = [];
+            for(var form in forms) {
+                if(forms[form].userid.localeCompare(userId) == 0) {
+                    formsForUser.push(forms[form]);
+                }
+            }
+            callback(formsForUser);
         }
 
-        function findAllFormsForUser(userId, callback) {
-
+        function deleteFormById(formId, callback) {
+            for(var form in forms) {
+                if(forms[form].formId.localeCompare(formId) == 0) {
+                    forms.splice(formId, 1);
+                }
+            }
+           callback(forms);
         }
 
         function updateFormById(formId, newForm, callback) {
+            for(var form in forms) {
+               if(forms[form].formId.localeCompare(formId) == 0) {
+                    forms[form].username = newForm.username;
+                    forms[form].password = newForm.password;
+                    forms[form].email = newForm.email;
+                    forms[form].firstName = newForm.firstName;
+                    forms[form].lastName = newForm.lastName;
+                    callback(forms[form]);
+                }
+            }
+            callback(newForm);
 
         }
     }

d5aa34ba1ffa39c742f444a1c50b742921b446f4
Divya Devaraj <divya@Divyas-MacBook-Pro.local>
2015-11-13 00:05:10 -0800

    testing rhc server

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));//host the static content in public directory
 
+var ipaddress 	= process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port 		= process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
 //bodyparser for update and create
 var bodyParser = require('body-parser');
 var multer = require('multer'); // v1.0.5
@@ -11,8 +14,5 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 app.use(multer());//for parsing multipart/form-data
 
 require("./public/assignment/server/app.js")(app);
-
-var ipaddress 	= process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port 		= process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 app.listen(port,ipaddress);

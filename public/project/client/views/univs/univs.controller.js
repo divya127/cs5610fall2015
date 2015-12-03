eb80fcdf59db044958d295d9df76a87e5fd1f3dc
Divya Devaraj <divya@Divyas-MacBook-Pro.local>
2015-12-02 22:46:07 -0800

    third party api integrated

--- public/project/client/views/univs/univs.controller.js
+++ public/project/client/views/univs/univs.controller.js
@@ -4,15 +4,21 @@
 		.module("AcademiaApp")
 		.controller("UnivController", UnivController);
 
-	function UnivController (UnivService) {
+	function UnivController (UnivService, $sce) {
 		var model = this;
 		model.search = search;
+		model.navigate = navigate;
 
 		function search () {
 			UnivService.findUnivByName(model.name).then(function(response){
 				model.results = response;
 				console.log(response);
 			});
+		}
+
+		function navigate(url) {
+			console.log("link model function");
+			return $sce.trustAsResourceUrl(url);
 		}
 	}
 }) ();

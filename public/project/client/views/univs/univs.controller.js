8f706870c1a1f6883ebe88032493bb7b920b6f46
Divya Devaraj <divya@Divyas-MacBook-Pro.local>
2015-12-04 03:45:34 -0800

    pulled recommendation content

--- public/project/client/views/univs/univs.controller.js
+++ public/project/client/views/univs/univs.controller.js
@@ -17,11 +17,26 @@
 			});
 		}
 
-		function mail (univName) {
-		//$rootScope.curid
-			ProfileService.addUnivToAppliedList("123" , univName).then(function(response){
-				$location.url("/profile");
-			});
+		function mail (univName, adminurl) {
+		ProfileService.findProfileForUser("123").then(function(response){
+				var recos = response[0].recommendations;
+				var mailString = "";
+				for(var reco in recos){
+					mailString += "Professor Name: " + recos[reco].authorName + " \nrecommendation Letter: " + recos[reco].content + " ";
+				}
+
+				console.log("mailString " + mailString);
+				var mailObj = {
+					"to" : "admissions@"+adminurl,
+					"subject" : "Recommendation Letters for " + "Devaraj" + " " + "Divya",//$rootScope.user.firstName,
+					"content" : mailString
+				};
+
+				ProfileService.addUnivToAppliedList("123" , univName).then(function(response){
+                				$location.url("/profile");
+                			});
+
+			})
 		}
 
 	}
